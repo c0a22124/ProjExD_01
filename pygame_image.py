7c8003e60a5b91b03c871a6f0b04a5d3f94c7dbf
yuki <c0a2212499@edu.teu.ac.jp>
2023-06-13 17:20:07 +0900

    演習

--- pygame_image.py
+++ pygame_image.py
@@ -7,19 +7,25 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img,True,False)
+
     kouka_img = pg.image.load("ex01/fig/3.png") 
     kouka_img = pg.transform.flip(kouka_img,True,False)
     kouka_img2 = pg.transform.rotozoom(kouka_img,10,1.0)
-    img_lst = [kouka_img,kouka_img2]
-
+    img_lst = []
+    for  i in range(10):
+        img_lst.append(pg.transform.rotozoom(kouka_img,i,1.0))
+    for j in range(9):
+        img_lst.append(pg.transform.rotozoom(kouka_img,9-j,1.0))
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        if tmr >800:
-            tmr = 0
-        screen.blit(bg_img, [-tmr, 0])
-        screen.blit(img_lst[tmr%2],[300,200])
+        
+
+        screen.blit(bg_img, [-1*(tmr%2400), 0])
+        screen.blit(bg_img2, [-1*(tmr%2400)+1600, 0])
+        screen.blit(img_lst[tmr%19],[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(100)
