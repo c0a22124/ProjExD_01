cfdfe45fb5d520a7ac492cdc5c84d524278505a4
yuki <c0a2212499@edu.teu.ac.jp>
2023-06-13 15:23:36 +0900

    問題２

---
+++ pygame_image.py
@@ -0,0 +1,28 @@
+import sys
+import pygame as pg
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    kouka_img = pg.image.load("ex01/fig/3.png") 
+    kouka_img = pg.transform.flip(kouka_img,True,False)
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(kouka_img,[100,100])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(10)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
